139fc17233f5e0eef5e8bc783231989d8592982d
Dan Thompson (SBS) <danthom@microsoft.com>
2024-05-13 06:06:40 -0700

    load and call the plugin

--- NativeHost/NativeHost.cpp
+++ NativeHost/NativeHost.cpp
@@ -1,8 +1,32 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 #include <stdio.h>
+
+typedef int (*DoSomethingFunction)(int);
 
 int main( int numArgs, wchar_t* args )
 {
-    wprintf( L"Hey there!" );
+    wprintf( L"Hey there!\n" );
+
+    // Let's load a plugin.
+
+    HMODULE hPlugin = LoadLibraryW( L"Plugin.dll" );
+
+    wprintf( L"Plugin loaded: %p\n", hPlugin );
+
+    if( hPlugin )
+    {
+        DoSomethingFunction dsf = reinterpret_cast<DoSomethingFunction>( GetProcAddress( hPlugin, "DoSomething" ) );
+
+        wprintf( L"Got DoSomething: %p\n", dsf );
+
+        if( dsf )
+        {
+            wprintf( L"Calling DoSomething...\n" );
+            int result = dsf( 42 );
+            wprintf( L"DoSomething returned: %d\n", result );
+        }
+    }
 
     return 0;
 }
